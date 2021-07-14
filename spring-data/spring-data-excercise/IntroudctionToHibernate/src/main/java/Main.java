d724a44475a11de66e677341f871326291431107
Stefan Gulev <stefangulev@gmail.com>
2021-07-15 02:36:16 +0300

    spring data

--- spring-data/spring-data-excercise/IntroudctionToHibernate/src/main/java/Main.java
+++ spring-data/spring-data-excercise/IntroudctionToHibernate/src/main/java/Main.java
@@ -9,26 +9,26 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+    private static final EntityManagerFactory factory = Persistence.createEntityManagerFactory("soft_uni");
+    private static final EntityManager em = factory.createEntityManager();
+    private static final Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("soft_uni");
-        EntityManager em = factory.createEntityManager();
-        Scanner scan = new Scanner(System.in);
         System.out.println("Enter problem number here:");
         int problemNumber = Integer.parseInt(scan.nextLine());
 
         switch (problemNumber) {
-            case 2: problemTwo(em); break;
-            case 3: problemThree(em, scan); break;
-            case 4: problemFour(em); break;
-            case 5: problemFive(em); break;
-            case 6: problemSix(em, scan); break;
-            case 7: problemSeven(em); break;
-            case 8: problemEight(em, scan); break;
-            case 9: problemNine(em); break;
-            case 10: problemTen(em); break;
-            case 11: problemEleven(em, scan); break;
-            case 12: problemTwelve(em); break;
-            case 13: problemThirteen(em,scan); break;
+            case 2: problemTwo(); break;
+            case 3: problemThree(); break;
+            case 4: problemFour(); break;
+            case 5: problemFive(); break;
+            case 6: problemSix(); break;
+            case 7: problemSeven(); break;
+            case 8: problemEight(); break;
+            case 9: problemNine(); break;
+            case 10: problemTen(); break;
+            case 11: problemEleven(); break;
+            case 12: problemTwelve(); break;
+            case 13: problemThirteen(); break;
             default: break;
 
         }
@@ -39,7 +39,7 @@ public class Main {
 
 
 
-    public static void problemTwo(EntityManager em) {
+    public static void problemTwo() {
        em.getTransaction().begin();
         TypedQuery<Town> getAllTowns = em.createQuery("SELECT t FROM Town t", Town.class);
         List<Town> collection = getAllTowns.getResultList();
@@ -57,35 +57,35 @@ public class Main {
 
     }
 
-    private static void problemThree(EntityManager em, Scanner scan) {
-        String result = getListOfEmployeesByCriteria("fullname", em, scan).size() >= 1 ? "Yes" : "No";
+    private static void problemThree() {
+        String result = getListOfEmployeesByCriteria("fullname").size() >= 1 ? "Yes" : "No";
         System.out.println(result);
 
     }
 
-    private static void problemFour(EntityManager em) {
-        getEmployeesWithSalaryMoreThan(50000, em).forEach(e -> System.out.println(e.getFirstName()));
+    private static void problemFour() {
+        getEmployeesWithSalaryMoreThan(50000).forEach(e -> System.out.println(e.getFirstName()));
     }
 
-    private static void problemFive(EntityManager em) {
+    private static void problemFive() {
         TypedQuery<Department> getDepartId = em.createQuery("SELECT d FROM Department d WHERE d.name = :name", Department.class);
         getDepartId.setParameter("name", "Research and Development");
         int id = getDepartId.getSingleResult().getId();
-        List<Employee> employees = getEmployeesByDepartmentId(id, em);
+        List<Employee> employees = getEmployeesByDepartmentId(id);
         employees.forEach(e -> System.out.println(String.format("%s %s from %s - ", e.getFirstName(), e.getLastName(), e.getDepartment().getName()) + "$" + e.getSalary()));
 
     }
-    private static void problemSix(EntityManager em, Scanner scan) {
+    private static void problemSix() {
         em.getTransaction().begin();
         Address address = new Address();
         address.setText("Vitoshka 15");
         em.persist(address);
-        Employee employee = getSingleEmployeeByCriteria("lastname", em, scan);
+        Employee employee = getSingleEmployeeByCriteria("lastname");
         employee.setAddress(address);
         em.persist(employee);
         em.getTransaction().commit();
     }
-    private static void problemSeven(EntityManager em) {
+    private static void problemSeven() {
         TypedQuery<Address> query = em.createQuery("SELECT a as count FROM Address a", Address.class);
         List<Address> resultList = query.getResultList();
         resultList.stream().sorted((l,r) -> Integer.compare(r.getEmployees().size(), l.getEmployees().size())).limit(10)
@@ -93,7 +93,7 @@ public class Main {
 
 
     }
-    private static void problemEight(EntityManager em, Scanner scan) {
+    private static void problemEight() {
         int id = Integer.parseInt(scan.nextLine());
         Employee employee = em.find(Employee.class, id);
       String info = String.format("%s %s - %s%n", employee.getFirstName(), employee.getLastName(), employee.getJobTitle());
@@ -101,12 +101,12 @@ public class Main {
         System.out.println(info + projects);
 
     }
-    private static void problemNine(EntityManager em) {
+    private static void problemNine() {
         TypedQuery<Project> query = em.createQuery("SELECT p FROM Project p ORDER BY p.startDate DESC", Project.class);
         System.out.println(query.getResultList().stream().limit(10).sorted(Comparator.comparing(Project::getName)).map(e -> String.
                 format("Project name: %s%n   Project Description: %s%n   Project Start Date: %s%n   Project End Date: %s%n", e.getName(), e.getDescription(), e.getStartDate().toString(), e.getEndDate() == null ? null : e.getEndDate().toString())).collect(Collectors.joining("")));
     }
-    private static void problemTen(EntityManager em) {
+    private static void problemTen() {
         em.getTransaction().begin();
         List<String> deptNames = Arrays.asList("Engineering", "Tool Design", "Marketing","Information Services");
       List<Employee> employees =  em.createQuery("SELECT e FROM Employee e ORDER BY e.department.id", Employee.class)
@@ -117,7 +117,7 @@ public class Main {
                em.getTransaction().commit();
 
     }
-    private static void problemEleven(EntityManager em, Scanner scan) {
+    private static void problemEleven() {
         String pattern = scan.nextLine();
         pattern += "%";
         TypedQuery<Employee> query = em.createQuery("SELECT e FROM Employee e WHERE e.firstName LIKE :pattern", Employee.class);
@@ -125,14 +125,14 @@ public class Main {
         query.getResultList().forEach(e -> System.out.printf("%s %s - %s - ($%.2f)%n", e.getFirstName(), e.getLastName(), e.getJobTitle(), Double.parseDouble(e.getSalary().toString())));
 
     }
-    private static void problemTwelve(EntityManager em) {
+    private static void problemTwelve() {
         TypedQuery<Department> query = em.createQuery("SELECT d FROM Department d", Department.class);
         List<String> collect = query.getResultList().stream().filter(e -> getMaxSalaryFromDepartment(e).get() < 30000 || getMaxSalaryFromDepartment(e).get() > 70000)
                 .map(e -> String.format("%s %.2f", e.getName(), getMaxSalaryFromDepartment(e).get())).collect(Collectors.toList());
         collect.forEach(System.out::println);
 
     }
-    private static void problemThirteen(EntityManager em, Scanner scan) {
+    private static void problemThirteen() {
         em.getTransaction().begin();
         String townName = scan.nextLine();
        Town town = em.createQuery("SELECT t FROM Town t WHERE t.name = :name", Town.class).setParameter("name", townName).getSingleResult();
@@ -143,7 +143,7 @@ public class Main {
         em.getTransaction().commit();
     }
 
-    private static List<Employee> getListOfEmployeesByCriteria(String identifier , EntityManager em, Scanner scan) {
+    private static List<Employee> getListOfEmployeesByCriteria(String identifier) {
         String[] input = scan.nextLine().split(" ");
         TypedQuery<Employee> query;
         if(identifier.equals("fullname")) {
@@ -163,7 +163,7 @@ public class Main {
         return null;
 
     }
-    private static Employee getSingleEmployeeByCriteria(String identifier ,EntityManager em, Scanner scan) {
+    private static Employee getSingleEmployeeByCriteria(String identifier) {
         String[] input = scan.nextLine().split(" ");
         TypedQuery<Employee> query;
         if(identifier.equals("fullname")) {
@@ -183,14 +183,14 @@ public class Main {
         return null;
 
     }
-    private static List<Employee> getEmployeesWithSalaryMoreThan(double salary, EntityManager em) {
+    private static List<Employee> getEmployeesWithSalaryMoreThan(double salary) {
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery criteria = builder.createQuery();
         Root<Employee> r = criteria.from(Employee.class);
         criteria.select(r).where(builder.gt(r.get("salary"), salary));
         return em.createQuery(criteria).getResultList();
     }
-    private static List<Employee> getEmployeesByDepartmentId(int id, EntityManager em) {
+    private static List<Employee> getEmployeesByDepartmentId(int id) {
         TypedQuery<Employee> query = em.createQuery("SELECT e FROM Employee e WHERE e.department.id = :id ORDER BY e.salary, e.id", Employee.class);
         query.setParameter("id", id);
         return query.getResultList();
