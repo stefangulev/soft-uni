5ce1beaef8fa157cef3defec6d400870934582b8
Stefan Gulev <stefangulev@gmail.com>
2021-11-21 18:55:44 +0200

    Adding latest changes

--- ManchesterUnitedFan/src/main/resources/static/js/matches-js.js
+++ ManchesterUnitedFan/src/main/resources/static/js/matches-js.js
@@ -10,22 +10,21 @@ fetch("https://api.football-data.org/v2/teams/66/matches" ,{
             console.log(infoElement)
             let homeTeamScore = infoElement.score.fullTime.homeTeam != null ? infoElement.score.fullTime.homeTeam : '';
             let awayTeamScore = infoElement.score.fullTime.awayTeam !=null ? infoElement.score.fullTime.awayTeam : '';
-            let homeTeamImgName = infoElement.homeTeam.name.replace(/ /g, "");
-            let awayTeamImgName = infoElement.awayTeam.name.replace(/ /g, "");
+            let matchDate = infoElement.utcDate.replace("T", " ").replace("Z", " ");
 
-            //TODO maybe  change picture names to IDs.
+
             let card =
-                '<div className="card bg-dark text-white" style="width: 18rem; margin: 50px 600px">' +
+                '<div className="card bg-dark text-white" style="width: 18rem; margin: 50px 600px;">' +
                 '<img src="' + infoElement.competition.area.ensignUrl + '" style="display: inline-block" width="30px" height="30px">' +
                 '<h6 className="card-text" style="display: inline-block" width="50px" height="50px">' + infoElement.competition.name + '</h6>' +
-                '<div className="card-img-overlay">' +
+                '<div>' +
                 '<h5 className="card-title">' + infoElement.homeTeam.name + ' - ' + infoElement.awayTeam.name +'</h5>' +
-                '<img ' + 'src="/images/' + homeTeamImgName + '.png" ' + 'style="display: inline-block" width="100px" height="100px">' +
+                '<img ' + 'src="https://crests.football-data.org/' + infoElement.homeTeam.id + '.svg" ' + 'style="display: inline-block" width="100px" height="100px" alt="Team badge inavailable">' +
                 '<h2 style="display: inline-block">' + homeTeamScore + '</h2>' +
                 '<h2 style="display: inline-block">' + ' : ' + '</h2>' +
                 '<h2 style="display: inline-block">' + awayTeamScore + '</h2>' +
-                '<img ' + 'src="/images/' + awayTeamImgName + '.png" style="display: inline-block" width="100px" height="100px">' +
-                '<h6 className="card-text">Date (UTC):' + infoElement.utcDate + '</h6>' +
+                '<img ' + 'src="https://crests.football-data.org/' + infoElement.awayTeam.id + '.svg" ' + 'style="display: inline-block" width="100px" height="100px" alt="Team badge inavailable">' +
+                '<h6 className="card-text">Date (UTC):' + matchDate + '</h6>' +
                 '   </div>' +
                 '</div> ';
             $("#matches").append(card);
